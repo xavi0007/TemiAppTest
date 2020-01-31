c0ec515b7c82c132f4c44025bfd0ea4190e5a7aa
Xavier Tan <xavi0007@e.ntu.edu.sg>
2020-01-31 11:22:42 +0800

    test

---
+++ app/src/main/java/com/example/axus/temiapptest/MainActivity.java
@@ -0,0 +1,432 @@
+package com.example.axus.temiapptest;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.content.pm.ActivityInfo;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.os.Bundle;
+import android.os.Environment;
+import android.util.Log;
+import android.view.View;
+import android.view.WindowManager;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.AdapterView;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Toast;
+
+import com.robotemi.sdk.BatteryData;
+import com.robotemi.sdk.MediaObject;
+import com.robotemi.sdk.NlpResult;
+import com.robotemi.sdk.Robot;
+import com.robotemi.sdk.TtsRequest;
+import com.robotemi.sdk.activitystream.ActivityStreamObject;
+import com.robotemi.sdk.activitystream.ActivityStreamPublishMessage;
+import com.robotemi.sdk.listeners.OnBeWithMeStatusChangedListener;
+import com.robotemi.sdk.listeners.OnConstraintBeWithStatusChangedListener;
+import com.robotemi.sdk.listeners.OnDetectionStateChangedListener;
+import com.robotemi.sdk.listeners.OnGoToLocationStatusChangedListener;
+import com.robotemi.sdk.listeners.OnLocationsUpdatedListener;
+import com.robotemi.sdk.listeners.OnRobotReadyListener;
+
+import org.jetbrains.annotations.NotNull;
+
+import java.io.File;
+import java.io.FileOutputStream;
+import java.util.List;
+
+public class MainActivity extends AppCompatActivity implements Robot.NlpListener, OnRobotReadyListener, Robot.ConversationViewAttachesListener, Robot.WakeupWordListener, Robot.ActivityStreamPublishListener, Robot.TtsListener, OnBeWithMeStatusChangedListener, OnGoToLocationStatusChangedListener, OnLocationsUpdatedListener, OnDetectionStateChangedListener, Robot.AsrListener, OnConstraintBeWithStatusChangedListener {
+
+
+    //variable declaration
+    private Robot robot;
+    List<String> locations;
+
+    //LinearLayout rLL = (LinearLayout) findViewById(R.id.main_layout);
+    //UI
+    public EditText etSpeak, etSaveLocation, etGoTo;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_HIDDEN);
+        initViews();
+        //because static
+        initiator.verifyStoragePermissions(this);
+        robot = Robot.getInstance(); // get an instance of the robot in order to begin using its features.
+    }
+
+
+    public void initViews() {
+        etSpeak = findViewById(R.id.etSpeak);
+        etSaveLocation = findViewById(R.id.etSaveLocation);
+        etGoTo = findViewById(R.id.etGoTo);
+    }
+
+    protected void onStart() {
+        super.onStart();
+        robot.addOnRobotReadyListener(this);
+        robot.addNlpListener(this);
+        robot.addOnBeWithMeStatusChangedListener(this);
+        robot.addOnGoToLocationStatusChangedListener(this);
+        robot.addConversationViewAttachesListenerListener(this);
+        robot.addWakeupWordListener(this);
+        robot.addTtsListener(this);
+        robot.addOnLocationsUpdatedListener(this);
+        robot.addOnConstraintBeWithStatusChangedListener(this);
+        robot.addOnDetectionStateChangedListener(this);
+        robot.addAsrListener(this);
+    }
+
+    protected void onStop() {
+        super.onStop();
+        super.onStop();
+        robot.removeOnRobotReadyListener(this);
+        robot.removeNlpListener(this);
+        robot.removeOnBeWithMeStatusChangedListener(this);
+        robot.removeOnGoToLocationStatusChangedListener(this);
+        robot.removeConversationViewAttachesListenerListener(this);
+        robot.removeWakeupWordListener(this);
+        robot.removeTtsListener(this);
+        robot.removeOnLocationsUpdateListener(this);
+        robot.removeDetectionStateChangedListener(this);
+        robot.removeAsrListener(this);
+        robot.stopMovement();
+    }
+
+    public static void hideKeyboard(Activity activity) {
+        InputMethodManager imm = (InputMethodManager) activity.getSystemService(Activity.INPUT_METHOD_SERVICE);
+        //Find the currently focused view, so we can grab the correct window token from it.
+        View view = activity.getCurrentFocus();
+        //If no view currently has focus, create a new one, just so we can grab a window token from it
+        if (view == null) {
+            view = new View(activity);
+        }
+        imm.hideSoftInputFromWindow(view.getWindowToken(), 0);
+    }
+
+    /**
+     * Have the robot speak while displaying what is being said.
+     */
+    public void speak(View view) {
+        TtsRequest ttsRequest = TtsRequest.create(etSpeak.getText().toString().trim(), true);
+        robot.speak(ttsRequest);
+        hideKeyboard(MainActivity.this);
+    }
+
+    /**
+     * goTo checks that the location sent is saved then goes to that location.
+     */
+    public void goTo(View view) {
+        for (String location : robot.getLocations()) {
+            if (location.equals(etGoTo.getText().toString().toLowerCase().trim())) {
+                robot.goTo(etGoTo.getText().toString().toLowerCase().trim());
+                hideKeyboard(MainActivity.this);
+            }
+        }
+    }
+
+    /**
+     * stopMovement() is used whenever you want the robot to stop any movement
+     * it is currently doing.
+     */
+    public void stopMovement(View view) {
+        robot.stopMovement();
+        robot.speak(TtsRequest.create("Stopping", true));
+    }
+
+    /**
+     * Simple follow me example.
+     */
+    public void followMe(View view) {
+        robot.beWithMe();
+        hideKeyboard(MainActivity.this);
+    }
+
+    /**
+     * Manually navigate the robot with skidJoy, tiltAngle, turnBy and tiltBy.
+     * skidJoy moves the robot exactly forward for about a second. It controls both
+     * the linear and angular velocity. Float numbers must be between -1.0 and 1.0
+     */
+    public void skidJoy(View view) {
+        long t = System.currentTimeMillis();
+        long end = t + 1000;
+        while (System.currentTimeMillis() < end) {
+            robot.skidJoy(1F, 0F);
+        }
+    }
+    /**
+     * tiltAngle controls temi's head by specifying which angle you want
+     * to tilt to and at which speed.
+     */
+    public void tiltAngle(View view) {
+        robot.tiltAngle(23, 5.3F);
+    }
+
+    /**
+     * turnBy allows for turning the robot around in place. You can specify
+     * the amount of degrees to turn by and at which speed.
+     */
+    public void turnBy(View view) {
+        robot.turnBy(180, 6.2F);
+    }
+
+    /**
+     * tiltBy is used to tilt temi's head from its current position.
+     */
+    public void tiltBy(View view) {
+        robot.tiltBy(70, 1.2F);
+    }
+
+    /**
+     * getBatteryData can be used to return the current battery status.
+     */
+    public void getBatteryData(View view) {
+        BatteryData batteryData = robot.getBatteryData();
+        if (batteryData.isCharging()) {
+            TtsRequest ttsRequest = TtsRequest.create(batteryData.getBatteryPercentage() + " percent battery and charging.", true);
+            robot.speak(ttsRequest);
+        } else {
+            TtsRequest ttsRequest = TtsRequest.create(batteryData.getBatteryPercentage() + " percent battery and not charging.", true);
+            robot.speak(ttsRequest);
+        }
+    }
+
+    /**
+     * Display the saved locations in a dialog
+     */
+
+    @Override
+    public void onPublish(@NotNull ActivityStreamPublishMessage activityStreamPublishMessage) {
+
+    }
+
+    @Override
+    public void onConversationAttaches(boolean b) {
+
+    }
+
+    @Override
+    public void onNlpCompleted(@NotNull NlpResult nlpResult) {
+        //do something with nlp result. Base the action specified in the AndroidManifest.xml
+        Toast.makeText(MainActivity.this, nlpResult.action, Toast.LENGTH_SHORT).show();
+
+        switch (nlpResult.action) {
+            case "home.welcome":
+                robot.tiltAngle(23, 5.3F);
+                break;
+
+            case "home.dance":
+                long t = System.currentTimeMillis();
+                long end = t + 5000;
+                while (System.currentTimeMillis() < end) {
+                    robot.skidJoy(0F, 1F);
+                }
+                break;
+
+            case "home.sleep":
+                robot.goTo("home base");
+                break;
+        }
+    }
+
+    @Override
+    public void onTtsStatusChanged(@NotNull TtsRequest ttsRequest) {
+
+    }
+
+    @Override
+    public void onWakeupWord(@NotNull String s, int i) {
+
+    }
+
+    @Override
+    public void onBeWithMeStatusChanged(@NotNull String s) {
+        //  When status changes to "lock" the robot recognizes the user and begin to follow.
+        switch (s) {
+            case "abort":
+                // do something i.e. speak
+                robot.speak(TtsRequest.create("Abort", false));
+                break;
+
+            case "calculating":
+                robot.speak(TtsRequest.create("Calculating", false));
+                break;
+
+            case "lock":
+                robot.speak(TtsRequest.create("Lock", false));
+                break;
+
+            case "search":
+                robot.speak(TtsRequest.create("search", false));
+                break;
+
+            case "start":
+                robot.speak(TtsRequest.create("Start", false));
+                break;
+
+            case "track":
+                robot.speak(TtsRequest.create("Track", false));
+                break;
+        }
+    }
+
+    @Override
+    public void onGoToLocationStatusChanged(@NotNull String location, @NotNull String status, int descriptionId, @NotNull String description) {
+        Log.d("GoToStatusChanged", "descriptionId=" + descriptionId + ", description=" + description);
+        switch (status) {
+            case "start":
+                robot.speak(TtsRequest.create("Starting", false));
+                break;
+
+            case "calculating":
+                robot.speak(TtsRequest.create("Calculating", false));
+                break;
+
+            case "going":
+                robot.speak(TtsRequest.create("Going", false));
+                break;
+
+            case "complete":
+                robot.speak(TtsRequest.create("Completed", false));
+                break;
+
+            case "abort":
+                robot.speak(TtsRequest.create("Cancelled", false));
+                break;
+        }
+    }
+
+    @Override
+    public void onLocationsUpdated(@NotNull List<String> list) {
+
+    }
+
+    @Override
+    public void onRobotReady(boolean isReady) {
+        if (isReady) {
+            try {
+                final ActivityInfo activityInfo = getPackageManager().getActivityInfo(getComponentName(), PackageManager.GET_META_DATA);
+                robot.onStart(activityInfo);
+            } catch (PackageManager.NameNotFoundException e) {
+                throw new RuntimeException(e);
+            }
+        }
+    }
+
+    @Override
+    public void onAsrResult(@NotNull String s) {
+
+    }
+
+    @Override
+    public void onDetectionStateChanged(int i) {
+
+    }
+
+    @Override
+    public void onConstraintBeWithStatusChanged(boolean b) {
+
+    }
+
+    public void savedLocationsDialog(View view) {
+        hideKeyboard(MainActivity.this);
+        locations = robot.getLocations();
+        final CustomAdapter customAdapter = new CustomAdapter(MainActivity.this, android.R.layout.simple_selectable_list_item, locations);
+        AlertDialog.Builder versionsDialog = new AlertDialog.Builder(MainActivity.this);
+        versionsDialog.setTitle("Saved Locations: (Click to delete the location)");
+        versionsDialog.setPositiveButton("OK", null);
+        versionsDialog.setAdapter(customAdapter, null);
+        AlertDialog dialog = versionsDialog.create();
+        dialog.getListView().setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+                final AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setMessage("Delete location \"" + customAdapter.getItem(position) + "\" ?");
+                builder.setPositiveButton("No thanks", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+                builder.setNegativeButton("Yes", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        String location = customAdapter.getItem(position);
+                        if (location == null) {
+                            return;
+                        }
+                        boolean result = robot.deleteLocation(location);
+                        if (result) {
+                            locations.remove(position);
+                            robot.speak(TtsRequest.create(location + "delete successfully!", false));
+                            customAdapter.notifyDataSetChanged();
+                        } else {
+                            robot.speak(TtsRequest.create(location + "delete failed!", false));
+                        }
+                    }
+                });
+                Dialog deleteDialog = builder.create();
+                deleteDialog.show();
+            }
+        });
+        dialog.show();
+    }
+
+    public void saveLocation(View view) {
+        String location = etSaveLocation.getText().toString().toLowerCase().trim();
+        boolean result = robot.saveLocation(location);
+        if (result) {
+            robot.speak(TtsRequest.create("I've successfully saved the " + location + " location.", true));
+        } else {
+            robot.speak(TtsRequest.create("Saved the " + location + " location failed.", true));
+        }
+        hideKeyboard(MainActivity.this);
+    }
+
+    /**
+     * publishToActivityStream takes an image stored in the resources folder
+     * and uploads it to the mobile application under the Activities tab.
+     */
+    public void publishToActivityStream(View view){
+        ActivityStreamObject activityStreamObject;
+        if (robot != null) {
+            final String fileName = "puppy.png";
+            Bitmap bm = BitmapFactory.decodeResource(getResources(), R.drawable.puppy);
+            File puppiesFile = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath(), fileName);
+            FileOutputStream fileOutputStream = null;
+            try {
+                fileOutputStream = new FileOutputStream(puppiesFile);
+                bm.compress(Bitmap.CompressFormat.PNG, 100, fileOutputStream);
+                fileOutputStream.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            activityStreamObject = ActivityStreamObject.builder()
+                    .activityType(ActivityStreamObject.ActivityType.PHOTO)
+                    .title("Puppy")
+                    .media(MediaObject.create(MediaObject.MimeType.IMAGE, puppiesFile))
+                    .build();
+
+            robot.shareActivityObject(activityStreamObject);
+            robot.speak(TtsRequest.create("Uploading Image", false));
+        }
+    }
+
+    public void hideTopBar(View view) {
+        robot.hideTopBar();
+    }
+
+    public void showTopBar(View view) {
+        robot.showTopBar();
+    }
+
+}
